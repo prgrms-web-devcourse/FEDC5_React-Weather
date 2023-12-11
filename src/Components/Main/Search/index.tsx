86e35b216b146d92e5baf4a472db005216c9b0a3
choi-ik <ci980704@naver.com>
2023-12-12 02:53:10 +0900

    ✨Feat: 메인페이지 컴포넌트 스타일링

--- src/Components/Main/Search/index.tsx
+++ src/Components/Main/Search/index.tsx
@@ -1,12 +1,18 @@
 import './index.scss';
+<<<<<<< HEAD
 import useModal from '../../../hooks/useModal';
 import Modal from '../../Common/Modal';
 import Icon from '../../../Components/Common/Icon';
 import SearchModal from '../../SearchModal';
+=======
+import Icon from '../../../Components/Common/Icon';
+
+>>>>>>> 1006392 (✨Feat: 메인페이지 컴포넌트 스타일링)
 function Search() {
   const { visible, modalRef, openModal, closeModal } = useModal();
 
   return (
+<<<<<<< HEAD
     <div className="main-header__outer">
       <div
         className="main-header__inner"
@@ -22,6 +28,19 @@ function Search() {
         closeModal={closeModal}>
         <SearchModal />
       </Modal>
+=======
+    <div className="main-header-outer">
+      <div className="main-header-inner">
+        <Icon
+          className="main-header-icon"
+          iconText="search"
+        />
+        <input
+          className="main-header-input"
+          placeholder="지역명을 입력해주세요."
+        />
+      </div>
+>>>>>>> 1006392 (✨Feat: 메인페이지 컴포넌트 스타일링)
     </div>
   );
 }
