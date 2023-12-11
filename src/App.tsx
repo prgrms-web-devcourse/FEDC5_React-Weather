3a0900e5bbf35f84a1c67ed8345828e8bbf1beaa
choi-ik <ci980704@naver.com>
2023-12-12 02:53:10 +0900

    ✨Feat: 메인페이지 컴포넌트 스타일링

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
 import './style/index.scss';
+=======
+import './App.scss';
+>>>>>>> 1006392 (✨Feat: 메인페이지 컴포넌트 스타일링)
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import Main from './Routes/Main';
