5227346d5b3b11b30160d465aef972288be37bf4
choi-ik <ci980704@naver.com>
2023-12-12 18:46:18 +0900

    API 요청 실패

---
+++ api/getWeather.ts
@@ -0,0 +1,35 @@
+// import type { VercelRequest, VercelResponse } from '@vercel/node';
+// import axios from 'axios';
+// import { GetApiProps } from '../src/Types/Api';
+// /*
+//   초단기 예보
+//   단기 예보
+// */
+
+// const { APIKEY, ENDPOINT } = process.env;
+
+// export default async function (req: VercelRequest, res: VercelResponse) {
+//   const {
+//     service,
+//     numOfRows,
+//     pageNo,
+//     dataType,
+//     base_date,
+//     base_time,
+//     nx,
+//     ny
+//   }: GetApiProps = req.body;
+
+//   const { data: responseValue } = await axios({
+//     url: `${ENDPOINT}/${service}?serviceKey=${APIKEY}&dataType=${dataType}&numOfRows=${numOfRows}&pageNo=${pageNo}&base_date=${base_date}&base_time=${base_time}&nx=${nx}&ny=${ny}`
+//   });
+
+//   res.status(200).json(responseValue);
+// }
+export default function (req, res) {
+  console.log('aaaaaaaaaaaaa', req);
+  res.status(200).json({
+    name: 'chldlr',
+    id: 123
+  });
+}
--- src/Components/Main/index.tsx
+++ src/Components/Main/index.tsx
@@ -3,8 +3,32 @@ import Header from './Header';
 import Search from './Search';
 import Temperature from './Temperature';
 import VeryShortTermForecast from './VeryShortTermForecast';
+import axios from 'axios';
 
 function MainPage() {
+  (async () => {
+    const res = await fetch('/api/getWeather', {
+      method: 'POST'
+    });
+
+    console.log(res);
+    const json = await res.json();
+  })();
+
+  // const a = async () => {
+  //   // const data = await axios.post('/api/getWeatherApi', {
+  //   //   service: 'getUltraSrtFcst',
+  //   //   numOfRows: '100',
+  //   //   pageNo: '1',
+  //   //   dataType: 'JSON',
+  //   //   base_date: '20231212',
+  //   //   base_time: '0630',
+  //   //   nx: '55',
+  //   //   ny: '127'
+  //   // });
+  //   // console.log('요청한 API 데이터 :', data);
+  // };
+
   return (
     <>
       <Search />
