15dd887235179422b310bccbbd724d9c67d3ff58
choi-ik <ci980704@naver.com>
2023-12-12 18:46:18 +0900

    API 요청 실패

---
+++ src/Types/Api.ts
@@ -0,0 +1,10 @@
+export interface GetApiProps {
+  service: string;
+  numOfRows: string;
+  pageNo: string;
+  dataType: string;
+  base_date: string;
+  base_time: string;
+  nx: string;
+  ny: string;
+}
