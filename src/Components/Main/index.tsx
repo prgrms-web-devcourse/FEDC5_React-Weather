c87a7ad37afd2ab8f2ba773bb811b6b2134bc80c
choi-ik <ci980704@naver.com>
2023-12-14 01:49:13 +0900

    ✨Feat: 날씨 정보 API 요청 Custom Hook 구현 및 메인 페이지 렌더링 시 api 요청 기능 추가

--- src/Components/Main/index.tsx
+++ src/Components/Main/index.tsx
@@ -3,9 +3,29 @@ import Header from './Header';
 import Search from './Search';
 import Temperature from './Temperature';
 import VeryShortTermForecast from './VeryShortTermForecast';
-import axios from 'axios';
+import useGetWeather from '../../hooks/useGetWeather';
+import { useEffect, useMemo, useState } from 'react';
 
 function MainPage() {
+  // 추후 들어올 x, y 좌표 값
+  const [location, setLocation] = useState({ x: '55', y: '127' });
+  // 아래 속성들이 변경될 때마다 API 요청 진행
+  const [requestData, setRequestData] = useState({
+    service: 'getUltraSrtFcst',
+    numOfRows: '60',
+    pageNo: '10',
+    dataType: 'JSON',
+    base_date: '20231213',
+    base_time: '0600',
+    nx: location.x,
+    ny: location.y
+  });
+
+  const getWeatherData = useGetWeather({ ...requestData });
+
+  useEffect(() => {
+    console.log(getWeatherData);
+  }, [requestData]);
   return (
     <>
       <Search />
