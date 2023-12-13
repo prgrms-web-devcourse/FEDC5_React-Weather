952564f38e22eadebcb998a41afa9a7420a9c0a6
choi-ik <ci980704@naver.com>
2023-12-13 18:32:00 +0900

    ✨Feat: 날씨 정보 API 요청 서버리스 함수 생성 및 Package.json type: module 추가

--- src/Components/Main/index.tsx
+++ src/Components/Main/index.tsx
@@ -6,29 +6,6 @@ import VeryShortTermForecast from './VeryShortTermForecast';
 import axios from 'axios';
 
 function MainPage() {
-  (async () => {
-    const res = await fetch('/api/getWeather', {
-      method: 'POST'
-    });
-
-    console.log(res);
-    const json = await res.json();
-  })();
-
-  // const a = async () => {
-  //   // const data = await axios.post('/api/getWeatherApi', {
-  //   //   service: 'getUltraSrtFcst',
-  //   //   numOfRows: '100',
-  //   //   pageNo: '1',
-  //   //   dataType: 'JSON',
-  //   //   base_date: '20231212',
-  //   //   base_time: '0630',
-  //   //   nx: '55',
-  //   //   ny: '127'
-  //   // });
-  //   // console.log('요청한 API 데이터 :', data);
-  // };
-
   return (
     <>
       <Search />
